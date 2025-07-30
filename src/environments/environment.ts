b38e34b6a41c2c609b7bfcecee7f8bb9a0cd50f1
Alexandre Dejean <alexandre.dejean@epitech.eu>
2025-07-30 15:33:45 +0200

    [FIX] backend url when deployed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    backendUrl: 'https://sijo-qcm-backend.purpleground-9e91f953.francecentral.azurecontainerapps.io/api'
+    backendUrl: 'https://sijo-qcm-backend.graycoast-ee506803.francecentral.azurecontainerapps.io/api'
   };
